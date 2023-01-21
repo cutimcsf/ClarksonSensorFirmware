43927519a53060cfb2019f5f617a40094f8f204b
Tim C. Sweeney-Fanelli <tim@zerobytellc.com>
2023-01-21 17:38:04 -0500

    Adding comments

--- peripherals/dac.c
+++ peripherals/dac.c
@@ -11,6 +11,12 @@
 
 #include "dac.h"
 
+/**
+ * DAC's interface is very simple -- it takes a single 16-bit value from 0x0000
+ * through 0xFFFF. The DAC's V_out will be it's V_in * (value / 0xFFFF)
+ *
+ * @param value
+ */
 void DAC_writeValue(const uint16_t value) {
   SPIDRV_MTransmitB(sl_spidrv_usart_spi_dac_handle, &value, 1);
 }
